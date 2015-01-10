fe509865f7ea1fd87af880288c7677a20a99c555
Miroslav Trninic <miroslav.trninic@gmail.com>
2015-01-10 13:55:20 +0100

    fixing bugs

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,10 +24,11 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(function(){
+$env = $app->detectEnvironment(array(
 
-    return getenv("APP_ENV") ? "production" : "local";
-});
+	'local' => array('ubuntu'),
+
+));
 
 /*
 |--------------------------------------------------------------------------
